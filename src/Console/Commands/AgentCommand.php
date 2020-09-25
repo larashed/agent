86a8a91fcc6f4d0069d7232c5f3a94612bf21acb
Ignas Bernotas <ignas@iber.lt>
2020-09-25 04:01:30 +0300

    TCP support, improved larashed:agent command with environment variable checks, macOS support

--- src/Console/Commands/AgentCommand.php
+++ src/Console/Commands/AgentCommand.php
@@ -8,6 +8,7 @@ use Larashed\Agent\Agent;
 use Larashed\Agent\AgentConfig;
 use Larashed\Agent\Api\LarashedApi;
 use Larashed\Agent\Console\GoAgent;
+use Larashed\Agent\Ipc\SocketClient;
 use Larashed\Agent\Trackers\ApplicationEnvironmentTracker;
 use Larashed\Agent\Trackers\ServerInformationTracker;
 
@@ -23,7 +24,11 @@ class AgentCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'larashed:agent {--no-update} {--socket= : Domain socket path} {--log-level=info}';
+    protected $signature = 'larashed:agent
+        {--no-update}
+        {--socket-type= : Socket type (unix or tcp)}
+        {--socket-address= : Socket address (path or TCP address)}
+        {--log-level=info}';
 
     /**
      * The console command description.
@@ -52,16 +57,7 @@ class AgentCommand extends Command
      */
     public function handle()
     {
-        if (!Agent::isEnabled()) {
-            $this->error('Larashed agent is disabled for this environment');
-            exit;
-        }
-
-        if (!$this->environmentIsSupported()) {
-            $this->error(PHP_OS . ' OS is not supported. Please contact us.');
-
-            return;
-        }
+        $this->performChecks();
 
         $this->environmentTracker = app(ApplicationEnvironmentTracker::class);
         $this->serverTracker = app(ServerInformationTracker::class);
@@ -98,13 +94,14 @@ class AgentCommand extends Command
 
     protected function runAgent()
     {
+        /** @var GoAgent $agent */
+        $agent = app(GoAgent::class);
         $logLevel = $this->option('log-level');
-
-        $agent = new GoAgent(app(AgentConfig::class));
-        $socket = $this->option('socket');
+        $socketType = $this->option('socket-type');
+        $socketAddress = $this->option('socket-address');
 
         if ($this->option('no-update')) {
-            $agent->run($socket, $logLevel);
+            $agent->run($socketType, $socketAddress, $logLevel);
 
             return;
         }
@@ -117,13 +114,57 @@ class AgentCommand extends Command
             }
         }
 
-        $agent->run($socket, $logLevel);
+        $agent->run($socketType, $socketAddress, $logLevel);
     }
 
     protected function environmentIsSupported()
     {
         $os = strtolower(PHP_OS);
 
-        return !Str::contains($os, ['dar', 'win']);
+        return Str::contains($os, ['linux', 'darwin']);
+    }
+
+    protected function performChecks()
+    {
+        if (!Agent::isEnabled()) {
+            $this->error('Larashed agent is disabled for this environment.');
+            exit;
+        }
+
+        if (!$this->environmentIsSupported()) {
+            $this->error(ucfirst(PHP_OS) . ' OS is not supported. Please contact us.');
+            exit;
+        }
+
+        /** @var SocketClient $client */
+        $client = app(SocketClient::class);
+
+        if (empty(config('app.env'))) {
+            $this->error('APP_ENV environment variable is missing a value.');
+            exit;
+        }
+
+        if (empty($client->getSocketType())) {
+            $this->error('LARASHED_TRANSPORT environment variable is missing a value.');
+            exit;
+        }
+
+        if ($client->usesUnixSocket() && empty($client->getSocketAddress())) {
+            $this->error('LARASHED_SOCKET_DIR environment variable is missing a value.');
+            exit;
+        }
+
+        if ($client->usesTcpSocket() && empty($client->getSocketAddress())) {
+            $this->error('LARASHED_TCP_ADDRESS environment variable is missing a value.');
+            exit;
+        }
+
+        /** @var AgentConfig $config */
+        $config = app(AgentConfig::class);
+
+        if (empty($config->getApplicationId()) || empty($config->getApplicationKey())) {
+            $this->error('LARASHED_APP_ID and LARASHED_APP_KEY environment variables are missing values.');
+            exit;
+        }
     }
 }
